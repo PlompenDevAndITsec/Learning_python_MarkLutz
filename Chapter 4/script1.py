0234e3a3851567587414fcdb98c2b753c5bbaee4
PlompenDevAndITsec <149263309+PlompenDevAndITsec@users.noreply.github.com>
2023-11-09 15:57:07 +0100

    Add files via upload

---
+++ Chapter 4/script1.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+x = 123
+y = 345
+
+print(x + y)  # Integer addition
+
+print(1.5 * 8)  # Floating-point multiplication
+
+print(2 ** 100)  # 2 to the power of 100.
+
+print(len(str(2 ** 10000)))
+
+print(math.pi)
+print(math.sqrt(81))
+
+print(int(random.random() * 10))  # Produces a random number between 0.X and 1 X10 converted to int
+
+
